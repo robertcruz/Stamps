6c93942a9b7b285ee4fcf597664a44b547b42d99
abaranovskyi <abaranovskyi@stamps.com>
2018-10-24 15:16:28 -0700

    WEBAPPAUTO-253 - Initial files

---
+++ features/support/sdc_apps/sdc_reports.rb
@@ -0,0 +1,27 @@
+module SdcReports
+
+  class << self
+    def loading
+      klass = Class.new(SdcPage) do
+        page_object(:loading) { { xpath: '//div[text()="Loading..."]' } }
+      end
+      klass.new.loading
+    end
+
+    def toolbar
+      Toolbar
+    end
+
+    def modals
+      Modals
+    end
+
+    def filter_panel
+      FilterPanel
+    end
+
+    def grid
+      Grid
+    end
+  end
+end
