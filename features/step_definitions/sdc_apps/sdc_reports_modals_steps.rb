4d4f0e045a5619fcdb199b20ee71e81f0130b6e6
abaranovskyi <abaranovskyi@stamps.com>
2018-11-06 12:31:29 -0800

    WEBAPPAUTO-253 - page objects and steps for reports toolbar and modals. Gherkin script

--- features/step_definitions/sdc_apps/sdc_reports_modals_steps.rb
+++ features/step_definitions/sdc_apps/sdc_reports_modals_steps.rb
@@ -25,3 +25,18 @@ Then /^close feedback modal on reports$/ do
   SdcReports.modals.feedback.x_btn.click
   step 'expect filters modal on reports is not present'
 end
+
+#welcome modal
+Then /^expect welcome modal on reports is present$/ do
+  expect(SdcReports.modals.welcome.title).to be_present
+end
+
+Then /^expect welcome modal on reports is not present$/ do
+  expect(SdcReports.modals.welcome.title).not_to be_present
+end
+
+Then /^close welcome modal on reports$/ do
+  welcome = SdcReports.modals.welcome
+  welcome.x_btn.click if welcome.present?
+  step 'expect welcome modal on reports is not present'
+end
--- features/support/sdc_apps/sdc_reports_modals.rb
+++ features/support/sdc_apps/sdc_reports_modals.rb
@@ -9,6 +9,10 @@ module SdcReports
         Feedback.new
       end
 
+      def welcome
+        Welcome.new
+      end
+
       class Filters < SdcPage
         page_object(:title) { {xpath: '//div[text()="Filters"]'} }
         page_object(:x_btn) { {xpath: '//img[contains(@class, "close")]'} }
@@ -17,6 +21,14 @@ module SdcReports
       class Feedback < SdcPage
         page_object(:title) { {xpath: '//div[text()="Feedback"]'} }
         page_object(:x_btn) { {xpath: '//*[contains(@class, "sdc-icon-mobile-close-light")]'} }
+      end
+
+      class Welcome < SdcPage
+        page_object(:title) { {xpath: '//*[text()="Welcome!"]'} }
+        page_object(:x_btn) { {xpath: '//*[contains(@class, "sdc-icon-mobile-close-light")]'} }
+        page_object(:next) { {xpath: '//*[text()="Next"]'} }
+        page_object(:close) { {xpath: '//*[text()="Close"]'} }
+        page_object(:learn_more) { {xpath: '//*[text()="Learn More"]'} }
       end
     end
   end
