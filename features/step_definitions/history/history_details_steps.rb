5e2a9b3ca7a42eebb6f66bea5ff6b4074fcbea78
alanand <aanand@stamps.com>
2018-11-15 02:30:28 -0800

    WEBAPPAUTO-534

--- features/step_definitions/history/history_details_steps.rb
+++ features/step_definitions/history/history_details_steps.rb
@@ -1,4 +1,19 @@
+Then /^expect print details on history is present$/ do
+  step 'wait while loading history details panel'
+  expect(SdcHistory.details.header.title).to be_present
+end
 
+Then /^expect return to on history print details contain (.*)$/ do |str|
+  expect(SdcHistory.details.addresses.return_to.text_value).to include(str)
+end
+
+Then /^wait while loading history details panel$/ do
+  SdcHistory.details.loading.safe_wait_until_present(timeout: 8)
+  SdcHistory.details.loading.wait_while_present(timeout: 240)
+end
+
+
+# Addresses
 
 Then /^expect address shipped to in transaction details displays saved address$/ do
   shipped_address = SdcHistory.details.addresses
@@ -21,6 +36,6 @@ end
 Then /^set search prints to (?:newly added|(.*)) tracking number on history filter panel$/ do |str|
   search =  SdcHistory.filter_panel.search
   search.search_prints.safe_wait_until_present(timeout: 10)
-  str = TestData.hash[:tracking_number]
+  str||= TestData.hash[:tracking_number]
   search.search_prints.set(str)
 end
