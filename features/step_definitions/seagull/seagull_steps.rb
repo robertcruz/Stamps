f2a85798f9cf8ae6582063a8719f7fdddd4e4e41
Rob Cruz <robert.cruz@gmail.com>
2018-11-16 12:22:27 -0800

    Refactoring Contacts Framework

--- features/step_definitions/seagull/seagull_steps.rb
+++ features/step_definitions/seagull/seagull_steps.rb
@@ -8,9 +8,10 @@ Then /^the enthusiast asks to see views of the car$/ do
 end
 
 Then /^he is shown all the available photographs$/ do
-  # SeaGull.car.back.wait_until_present(timeout: 10)
-  # expect(SeaGull.car.back.present?).to be true
-  # expect(SeaGull.car.side_closed.present?).to be true
-  # expect(SeaGull.car.front_above.present?).to be true
-  # expect(SeaGull.car.left_rear.present?).to be true
+  SeaGull.car.back.wait_until_present(timeout: 10)
+  expect(SeaGull.car.back.present?).to be true
+  expect(SeaGull.car.side_closed.present?).to be true
+  expect(SeaGull.car.front_above.present?).to be true
+  expect(SeaGull.car.left_rear.present?).to be true
+  sleep 2
 end
